b7610ce97358d5899b0784553d23280ef8a94826
Vladdzen <vladislavklimakov@yandex.ru>
2024-11-24 16:48:27 +0300

    Add files via upload

---
+++ module_5_4.py
@@ -0,0 +1,69 @@
+class house:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return super().__new__(cls)
+
+    def __del__(self):
+        print(f'{self.name} снесён, но он останется в истории')
+
+    def __init__(self,name,number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def go_to(self,new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print("Такого этажа не существует")
+        else:
+            for i in range(new_floor):
+                print(i+1)
+
+    def __eq__(self, other):
+        return f' Колличество этажей {self.name} и {other.name} равно? {self.number_of_floors == other.number_of_floors}'
+
+    def __lt__(self, other):
+        return f' Колличество этажей {self.name} и {other.name} меньше? {self.number_of_floors < other.number_of_floors}'
+
+    def __le__(self, other):
+        return f' Колличество этажей {self.name} и {other.name} меньше или равно? {self.number_of_floors <= other.number_of_floors}'
+
+    def __gt__(self, other):
+        return f' Колличество этажей {self.name} и {other.name} больше? {self.number_of_floors > other.number_of_floors}'
+
+    def __ge__(self, other):
+        return f' Колличество этажей {self.name} и {other.name} больше или равно? {self.number_of_floors >= other.number_of_floors}'
+
+    def __ne__(self, other):
+        return f' Колличество этажей {self.name} и {other.name} не равно? {self.number_of_floors != other.number_of_floors}'
+
+    def __add__(self, value):
+        self.number_of_floors += value
+        return self
+
+    def __radd__(self, value):
+        self.number_of_floors += value
+        return self
+
+    def __iadd__(self, value):
+        self.number_of_floors += value
+        return self
+
+h1 = house('ЖК Эльбрус', 10)
+print(house.houses_history)
+h2 = house('ЖК Акация', 20)
+print(house.houses_history)
+h3 = house('ЖК Матрёшки', 20)
+print(house.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(house.houses_history)
